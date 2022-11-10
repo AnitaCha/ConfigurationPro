b2b4ea6350d1479bd5a88175e88691bd8bf53ec9
anitaCha <anitachaudhari@gmail.com>
2022-11-10 15:55:36 +0530

    Application files

---
+++ src/main/java/com/example/demo/ConfigProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigProjectApplication.class, args);
+	}
+
+}
